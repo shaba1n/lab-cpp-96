3755dfc8300bee01b15d88b918a2e850c3a7311b
shaba1n <mdshaban3415@gmail.com>
2026-08-11 09:05:22 +0000

    commit

---
+++ code1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+int *arr;
+int size;
+cout<<"enter the size of array";
+cin>>size;
+cout<<"creating array";
+arr=new int [size];
+cout<<"enter value of arry";
+for(int i=0;i<size;i++)
+{
+    cin>>arr[i];
+}
+cout<<"display arr";
+for(int i=0;i<size;i++)
+{
+    cout<<""<<arr[i];
+}
+return 0;
+}
